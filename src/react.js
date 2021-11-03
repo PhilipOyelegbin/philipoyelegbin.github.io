05c6a7f1177442278349a9fe3854cf3f5be6f790
Philip K. Oyelegbin <72349309+PhilipOyelegbin@users.noreply.github.com>
2021-11-03 14:55:23 +0100

    Add files via upload

---
+++ src/react.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict'
+
+  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+})()
+
+function order() {
+  document.querySelector(".form-card").classList.add("show");
+};
+
+document.querySelector(".close-btn").addEventListener("click", function() {
+  document.querySelector(".form-card").classList.remove("show");
+});
