5d784e0c85e6a616729209dcd7b024aa7e4a566b
Philip K. Oyelegbin <72349309+PhilipOyelegbin@users.noreply.github.com>
2022-04-26 08:04:04 +0100

    Add files via upload

--- src/react.js
+++ src/react.js
@@ -1,15 +1,42 @@
 (function () {
-  'use strict'
+  "use strict";
 
-  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
-    document.querySelector('.offcanvas-collapse').classList.toggle('open')
-  })
-})()
+  document
+    .querySelector("#navbarSideCollapse")
+    .addEventListener("click", function () {
+      document.querySelector(".offcanvas-collapse").classList.toggle("open");
+    });
+})();
 
-function order() {
-  document.querySelector(".form-card").classList.add("show");
+// fading in and sliding in of columns
+const faders = document.querySelectorAll(".fade-in");
+
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -200px 0px",
 };
 
-document.querySelector(".close-btn").addEventListener("click", function() {
-  document.querySelector(".form-card").classList.remove("show");
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
 });
