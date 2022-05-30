31624b85a983f4623e93fbf9838b755dfefb93a7
Philip K. Oyelegbin <72349309+PhilipOyelegbin@users.noreply.github.com>
2022-05-30 11:02:45 +0100

    Add files via upload

---
+++ react.js
@@ -0,0 +1,68 @@
+// preloader action
+window.addEventListener("load", function () {
+  document.getElementById("loader").style.display = "none";
+});
+
+// menu button toggle
+document.getElementById("menu"),
+  addEventListener("click", function () {
+    document.getElementById("menuContent").classList.toggle("opacity-100");
+    document.getElementById("menuContent").classList.toggle("top-[-300px]");
+    document.getElementById("close").classList.toggle("block");
+    document.getElementById("harmbuger").classList.toggle("hidden");
+    document.getElementById("close").classList.toggle("hidden");
+  });
+
+// fading in and sliding in of columns
+const faders = document.querySelectorAll(".fade-in");
+
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -200px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
+
+// whatsapp chatbot
+(function () {
+  var options = {
+    whatsapp: "+2348054945601", // WhatsApp number
+    call_to_action: "Hire me", // Call to action
+    position: "right", // Position may be 'right' or 'left'
+  };
+  var proto = document.location.protocol,
+    host = "getbutton.io",
+    url = proto + "//static." + host;
+  var s = document.createElement("script");
+  s.type = "text/javascript";
+  s.async = true;
+  s.src = url + "/widget-send-button/js/init.js";
+  s.onload = function () {
+    WhWidgetSendButton.init(host, proto, options);
+  };
+  var x = document.getElementsByTagName("script")[0];
+  x.parentNode.insertBefore(s, x);
+})();
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["'Poppins'", "san-serif"],
+      },
+      backgroundImage: {
+        hero: "url('https://img.freepik.com/free-photo/it-specialist-checking-code-computer-dark-office-night_1098-18699.jpg?t=st=1653533480~exp=1653534080~hmac=ff288ff2fe22420f62bdaf3749a81a2440a25bb68de54789eefc28ff6465e70e&w=826')",
+      },
+    },
+  },
+  plugins: [],
+};
