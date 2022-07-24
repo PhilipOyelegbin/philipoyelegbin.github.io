b18323a2f12212468ed6d0a73d017ed65eb673a0
Philip Oyelegbin <philipoyelegbin@gmail.com>
2022-07-24 20:54:50 +0100

    project links updated

--- src/components/data.js
+++ src/components/data.js
@@ -41,15 +41,15 @@ export const skillsData = [
 ];
 
 export const projectData = [
-    {link: "https://demoboard.netlify.app", image: demoboard, description: "Demoboard - A dashboard task from Techathon intership created using useState and useEffect while storing data into sessionStorage"},
+    {link: "https://demoboard.netlify.app", image: demoboard, description: "Demoboard - A dashboard task from Techathon internship created using useState and useEffect while storing data into sessionStorage"},
     {link: "https://tradezon.netlify.app", image: tradezon, description: "Tradezon - An affiliate marketing website developed using React and Tailwind, carousel implemeted using slider.js"},
     {link: "https://nftprev.netlify.app", image: nftprev, description: "NFT - A design gotten from frontend-mentors developed using React and Tailwind"},
     {link: "https://myfylo.netlify.app", image: fylo, description: "Fylo - A design gotten from frontend-mentors created using React and Tailwind"},
     {link: "https://www.icrestinternational.com", image: icrest, description: "iCREST - A scholarship agency website developed using HTML, CSS(Tailwind) and JavaScript"},
     {link: "https://oyetek.herokuapp.com", image: oyetek, description: "OYETEK - An engineering and software solution provider website built with HTML, CSS(Bootstrap) and JavaScript"},
     {link: "https://philipoyelegbin.github.io/food-store", image: organic, description: "Organic Food Store - An on-line fruit store created using HTML, CSS(Bootstrap) and JavaScript"},
-    {link: "", image: webcounter, description: "Web Counter - Developed with HTML, CSS and JavaScript"},
-    {link: "", image: colorflipper, description: "Color Flipper - Developed with HTML, CSS and JavaScript"}
+    {link: "https://philipoyelegbin.github.io/webcounter", image: webcounter, description: "Web Counter - Developed with HTML, CSS and JavaScript"},
+    {link: "https://philipoyelegbin.github.io/color-flipper", image: colorflipper, description: "Color Flipper - Developed with HTML, CSS and JavaScript"}
 ];
 
 export const serviceData = [
