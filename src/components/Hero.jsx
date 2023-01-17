34189f67a06506ff4260f39ad17815719c94f85f
Philip Oyelegbin <philipoyelegbin@gmail.com>
2023-01-17 20:34:13 +0100

    fix: iamge loading issues and local images added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <header className="h-screen bg-hero bg-no-repeat bg-cover bg-center md:bg-right bg-fixed" id="home">
+    <header className="h-screen bg-[url('../src/assets/hero-bg.jpg')] bg-no-repeat bg-cover bg-center md:bg-right bg-fixed" id="home">
       <div className="flex flex-col justify-center items-center h-full bg-black bg-opacity-60 text-center px-5 mt-5 md:mt-0">
         <div className="flex flex-col justify-center gap-3 items-center md:w-3/5 animate__animated animate__fadeInUp animate__slow">
           <h1 className="font-extrabold text-3xl text-slate-300 md:text-5xl">Build a website that suits your business</h1>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        hero: "url('https://img.freepik.com/free-photo/portrait-artificial-intelligence-app-developer-sitting-down-typing-laptop-fixing-glasses-looking-up-smiling-camera-programer-using-portable-computer-innovating-cloud-computing_482257-40643.jpg?size=626&ext=jpg&ga=GA1.2.405634466.1654584137')"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
