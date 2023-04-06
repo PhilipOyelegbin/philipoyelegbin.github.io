2a9420fcb6903f1cbd1e7f6fcb872260ca6498bf
Philip Oyelegbin <philipoyelegbin@gmail.com>
2023-04-06 23:40:16 +0100

    fix: cors imported

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const projectRoute = require('./routes/projectRoutes');
 const feedbackRoute = require('./routes/feedbackRoutes');
 require('dotenv').config();
