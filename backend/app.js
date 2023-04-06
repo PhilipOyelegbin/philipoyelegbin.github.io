ccb4dec4f7e6a78630065b9dc33d9ce30a979411
Philip Oyelegbin <philipoyelegbin@gmail.com>
2023-04-06 23:29:15 +0100

    feat: cors added

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const morgan = require('morgan');
 const mongoose = require('mongoose');
 const projectRoute = require('./routes/projectRoutes');
 const feedbackRoute = require('./routes/feedbackRoutes');
@@ -22,8 +21,8 @@ app.set('view engine', 'ejs')
 // middleware
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({extended: false}))
-// app.use(morgan('dev'))
 
 // page route
 app.get("/", (req, res) => {
