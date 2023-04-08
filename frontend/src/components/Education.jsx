815d0393856f5db02d8ca255aee9d847f797e75f
Philip Oyelegbin <philipoyelegbin@gmail.com>
2023-04-08 12:59:57 +0100

    fix: github deployment issues

--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -1,22 +1,17 @@
-// import { EducationData } from "./data"
+import { EducationData } from "./data"
 
 const Education = () => {
-  const EducationData = [
-    {certificate: "Meta Frontend Developer", institution: "Coursera", duration: "2022 - 2023"},
-    {certificate: "HND, Industrial Maintenance Engineer", institution: "Yaba College of Technology", duration: "2019 - 2022"}
-  ]
-
   return (
     <section className="px-3 py-5 lg:px-20">
-        <div className="flex flex-wrap md:justify-center gap-5">
-            {EducationData?.map((education, index) => (
-                <div key={index} className="shadow-md shadow-gray-500 p-3 rounded-2xl ease-in-out duration-300 hover:-translate-y-2">
-                    <h3>{education.certificate}</h3>
-                    <p>{education.institution}</p>
-                    <p>{education.duration}</p>
-                </div>
-            ))}
-        </div>
+      <div className="flex flex-wrap md:justify-center gap-5">
+        {EducationData?.map((education, index) => (
+          <div key={index} className="shadow-md shadow-gray-500 p-3 rounded-2xl ease-in-out duration-300 hover:-translate-y-2">
+            <h3>{education.certificate}</h3>
+            <p>{education.institution}</p>
+            <p>{education.duration}</p>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -1,24 +1,17 @@
-// import { ExperienceData } from "./data"
+import { ExperienceData } from "./data"
 
 const Experience = () => {
-  const ExperienceData = [
-    {role: "Customer Support Representative", company: "Whogohost Limited", duration: "2022 - till date"},
-    {role: "Frontend Engineer (Volunteer)", company: "See9ja", duration: "2022 - till date"},
-    {role: "Robotics Instructor (Contract)", company: "WiFi Combat Academy", duration: "2021 - till date"},
-    {role: "Frontend Engineer (Volunteer)", company: "Valorise Limited", duration: "2022 - 2023"},
-  ]
-
   return (
     <section className="px-3 py-5 ld:px-20">
-        <div className="flex flex-wrap md:justify-center gap-5">
-            {ExperienceData?.map((education, index) => (
-                <div key={index} className="shadow-md shadow-gray-500 p-3 rounded-2xl ease-in-out duration-300 hover:-translate-y-2">
-                    <h3>{education.role}</h3>
-                    <p>{education.company}</p>
-                    <p>{education.duration}</p>
-                </div>
-            ))}
-        </div>
+      <div className="flex flex-wrap md:justify-center gap-5">
+        {ExperienceData?.map((education, index) => (
+          <div key={index} className="shadow-md shadow-gray-500 p-3 rounded-2xl ease-in-out duration-300 hover:-translate-y-2">
+            <h3>{education.role}</h3>
+            <p>{education.company}</p>
+            <p>{education.duration}</p>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
