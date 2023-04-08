e4f2190a54c12eaed971e51575a9b25cb7dc8633
Philip Oyelegbin <philipoyelegbin@gmail.com>
2023-04-08 17:35:05 +0100

    feat: portfolio rebuilt

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,7 +1,7 @@
 // import { Link } from "react-router-dom";
 import { FaInternetExplorer, FaGithub } from "react-icons/fa";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { getData } from "../hooks/useFetch";
 
