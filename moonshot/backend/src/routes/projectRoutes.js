d4840c6502832529a7f558c409ce76d35e34218a
Philip K. Oyelegbin <72349309+PhilipOyelegbin@users.noreply.github.com>
2024-04-01 19:13:53 +0100

    Delete moonshot directory

--- moonshot/backend/src/routes/projectRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const projectController = require('../controllers/projectControllers');
-
-router.get('/', projectController.get_project);
-
-module.exports = router;
