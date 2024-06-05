cb0035b9a01a20e349d177e49a4d50a5a6da6d99
PhilipOyelegbin <philipoyelegbin@gmail.com>
2024-06-05 02:53:40 +0100

    reconfigure cors for custom domain

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.DB_URL)
 // middleware
 app.use(express.json())
 app.use(helmet())
-app.use(cors({"origin": ["http://localhost:3000", "https://philipoyelegbin.com.ng", "https://philipoyelegbin.netlify.app"]}))
+app.use(cors({"origin": ["http://localhost:3000", "https://philipoyelegbin.com.ng", "https://philipoyelegbin.netlify.app", "*"]}))
 app.use(express.urlencoded({extended: false}))
 
 // page route
