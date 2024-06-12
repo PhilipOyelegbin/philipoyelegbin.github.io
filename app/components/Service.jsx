676ad2f82f3b2e20686ea82b79f6df07b65b5220
PhilipOyelegbin <philipoyelegbin@gmail.com>
2024-06-12 06:57:42 +0100

    image bug fixed

--- app/components/Service.jsx
+++ app/components/Service.jsx
@@ -1,27 +1,40 @@
-import { Services } from "../utils/data"
+import Image from "next/image";
+import { Services } from "../utils/data";
 
 const Service = () => {
   return (
-    <section className="text-center px-5 py-10 lg:px-20" id="service">
+    <section className='text-center px-5 py-10 lg:px-20' id='service'>
       <h2>ACTUALIZATION</h2>
-      <p className='lg:text-center'>Web design for businesses! From conception to product UI/UX and developing web pages, I've got you covered. <strong>Here's how it works</strong></p>
+      <p className='lg:text-center'>
+        Web design for businesses! From conception to product UI/UX and
+        developing web pages, I've got you covered.{" "}
+        <strong>Here's how it works</strong>
+      </p>
 
       {/* grid view of the services in a container */}
-      <div className="grid grid-flow-row grid-cols-1 gap-5 mt-5 lg:grid-cols-3 aspect-auto">
-        {Services?.map(service => {
+      <div className='grid grid-flow-row grid-cols-1 gap-5 mt-5 lg:grid-cols-3 aspect-auto'>
+        {Services?.map((service) => {
           return (
-            <figure className="card bg-gray-700 lg:h-full" key={service.id}>
-              <img className='mx-auto mt-5' src={service.icon || ""} alt="icon" />
-              <figcaption className="p-3">
-                <h3 className='text-xl text-center font-bold'>{service.title || ""}</h3>
+            <figure className='card bg-gray-700 lg:h-full' key={service.id}>
+              <Image
+                className='mx-auto mt-5'
+                src={service.icon || ""}
+                width={100}
+                height={100}
+                alt='icon'
+              />
+              <figcaption className='p-3'>
+                <h3 className='text-xl text-center font-bold'>
+                  {service.title || ""}
+                </h3>
                 <p>{service.summary || ""}</p>
               </figcaption>
             </figure>
-          )
+          );
         })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Service
+export default Service;
